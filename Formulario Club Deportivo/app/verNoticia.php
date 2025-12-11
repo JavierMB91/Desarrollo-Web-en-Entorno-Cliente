b0fe428ffccc894f78aad19f95b20e21d62de6b1
Javier Morilla Barrena <jmorillabarrena@gmail.com>
2025-12-11 20:27:00 +0100

    favicon

--- Formulario Club Deportivo/app/verNoticia.php
+++ Formulario Club Deportivo/app/verNoticia.php
@@ -14,6 +14,10 @@ $noticia = $stmt->fetch(PDO::FETCH_ASSOC);
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="shortcut icon" href="favicon/favicon.ico">
 <link rel="stylesheet" href="css/estilos.css">
 <title><?= htmlspecialchars($noticia["titulo"]) ?></title>
 </head>
