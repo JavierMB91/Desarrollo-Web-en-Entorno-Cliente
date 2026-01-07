b945209ed6044065dacdae486301eab1606f369b
Javier Morilla Barrena <jmorillabarrena@gmail.com>
2026-01-07 18:18:56 +0100

    1

--- Formulario Club Deportivo/app/js/nav.js
+++
@@ -1,6 +0,0 @@
-fetch('nav.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('nav').innerHTML = data;
-    })
-    .catch(err => console.error('Error cargando nav:', err));
